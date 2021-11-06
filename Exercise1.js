0e564327d7be0dfa51d3de7018cfee6cc1268092
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-11-06 16:01:06 +0530

    aim and procedure for ex1 and 2

--- Exercise1.js
+++ Exercise1.js
@@ -1,3 +1,12 @@
+/*
+Aim : To Write a node.js program to get files or directories of a directory in JSON format.
+Procedure : 
+1. Load the required Nodejs pacakges using "require"
+2. Pass the directory path ("./") and callback function in fs.readdir(path, callbackFunction) Method.
+3. The callback function should have error handling and result handling logic.
+4. Use JSON.stringfy() function to convert the array of files into JSON
+*/
+
 const fs=require('fs');
 fs.readdir('./', (err, files) => {
     let files_json = JSON.stringify({files})
--- Exercise2.js
+++ Exercise2.js
@@ -1,3 +1,12 @@
+/*
+Aim : To Write a node.js program to get a local IP address
+Procedure : 
+1. Import the OS package using "require" and store it in a variable 
+2. Get the list of all network interfaces using the networkInterfaces() method  
+3. Choose your connected netowrk interface from the network interface object 
+4. print the address property of the network interface object
+*/
+
 var os = require( 'os' );
 var networkInterfaces = os.networkInterfaces();
 var arr = networkInterfaces['en1']
