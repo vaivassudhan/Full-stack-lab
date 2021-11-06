0ce1f8652b3d6b9d677bd000d8162fc6a8446e00
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-11-06 12:20:19 +0530

    ex4 and 6

--- Exercise1.js
+++ Exercise1.js
@@ -2,6 +2,4 @@ const fs=require('fs');
 fs.readdir('./', (err, files) => {
     let files_json = JSON.stringify({files})
     console.log(files_json)
-    files.forEach(file => {
-       console.log(file);
-})});
+});
---
+++ Exercise4.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const mysql = require('mysql')
+const express = require('express')
+const bodyparser = require('body-parser')
+var app = express()
+app.use(bodyparser.json())
+var mysqlC = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'password12345',
+    database:'FS-LAB',
+    multipleStatements: true,
+});
+mysqlC.connect((err)=>{
+    if(!err){
+        console.log("Connected")
+    }
+    else{
+        console.log(err)
+    }
+})
+// Read 
+app.get('/students' , (req, res) => {
+    mysqlC.query('SELECT * FROM student', (err, rows, fields) => {
+    if (!err){res.end(JSON.stringify(rows))}
+    else
+    {console.log(err);res.end("error");}
+    })
+});
+// Create 
+app.get('/insert',(req,res) =>{
+    mysqlC.query('INSERT INTO student VALUES (3,"sudhan","CSE","9080185622")',(err)=>{
+        if(!err) res.end("inserted")
+        else res.end("Error")
+    })
+})
+// Delete
+app.get('/delete',(req,res) => {
+    mysqlC.query('DELETE FROM student where student_id="3"' , (err) => {
+        if(!err) res.end("deleted")
+        else res.end("Error deleting")
+    })
+})
+app.listen(5000)
