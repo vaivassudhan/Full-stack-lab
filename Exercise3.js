3acb33d2642dc033029957d5c6accff60698f221
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-11-06 16:07:59 +0530

    ex3 aim

--- Exercise3.js
+++ Exercise3.js
@@ -1,3 +1,14 @@
+/*
+Aim : To Write a node.js program to check request header for cookies.
+Procedure : 
+1. Import the http package using "require"
+2. use http.createServer() function to create a nodeJs server and pass in a callback function to handle user request 
+3. In the callback function, the user request is processed for cookie check using the function checkCookie 
+4. In the checkCookie function we check the request.headres.cookie property of the request object, 
+  if it is undefined there are no cookies
+5. we return the response to the user to notify whether the cookie is present or not  
+ by using response.writehead() (for headers) and response.end() (for message)
+*/
 var http = require('http');
 
 function checkCookie (request) {
