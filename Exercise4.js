1b15b501089d496c75727b07c0ca030dec69d0a5
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-11-06 16:42:26 +0530

    ex4 aim and procedure

--- Exercise4.js
+++ Exercise4.js
@@ -2,6 +2,19 @@
 to test 
 curl -i -X POST -H 'Content-Type: application/json' -d '{"id":4,"Name": "curl-test","dept":"cse","phone": "9080185622"}' http://localhost:5000/insert
 curl -i -X DELETE http://localhost:5000/delete/3
+Aim :
+1. to Build a Simple CRUD Node.js App with MySql Database connectivity 
+Procedure :
+1. Import mysql, express and bodypareser packages using "require" method and store it in respective variables 
+2. create an object of class express by calling its contructor 
+3. Add bodyparser to the express app by using app.use() method to parse JSON 
+4. create a mysql database connection by using createConnection() method of mysql package 
+    with parameter object containing host,use, password and database name
+5. Then connect to the database using connect() function of mysqlConnecctor variable 
+6. Create: listen with post method and get the data from request object and use the mysql query to insert the data into the database
+7. Read : listen with get method to select all records from the given table and return to the user as response 
+8. Update : listen with put method to get data from user and update the exisiting record using the update query
+8. Delete : listen with delete method to delete a record in the database using delete command  
 */
 
 const http = require('http')
@@ -50,8 +63,8 @@ app.put('/update',(req,res) =>{
     })
 })
 // Delete
-app.delete('/delete/:id',(req,res) => {
-    console.log(req.body.data)
+app.delete('/delete',(req,res) => {
+    console.log(req.body)
     mysqlC.query('DELETE FROM student where student_id="3"' , (err) => {
         if(!err) res.end("deleted")
         else res.end("Error deleting")
